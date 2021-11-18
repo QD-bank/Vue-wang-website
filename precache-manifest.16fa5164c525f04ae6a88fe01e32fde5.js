48aecc150bbb91aa96d50665acc9126b05c7f5c7
lijun@sh.woowis.com <lijun@sh.woowis.com>
2021-11-18 10:33:00 +0800

    deploy

---
+++ precache-manifest.16fa5164c525f04ae6a88fe01e32fde5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b2b36e9299a5fc463d",
+    "url": "/Vue-wang-website/css/app.bb4baf47.css"
+  },
+  {
+    "revision": "50bedf864146937ef42f9e0e9ea93d84",
+    "url": "/Vue-wang-website/index.html"
+  },
+  {
+    "revision": "62b2b36e9299a5fc463d",
+    "url": "/Vue-wang-website/js/app.8a6ef18d.js"
+  },
+  {
+    "revision": "b6cd98a02fe40fec1af6",
+    "url": "/Vue-wang-website/js/chunk-vendors.062bcb60.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/Vue-wang-website/manifest.json"
+  },
+  {
+    "revision": "196cad36453ca6975139a643cce3cbda",
+    "url": "/Vue-wang-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-wang-website/robots.txt"
+  }
+]);
